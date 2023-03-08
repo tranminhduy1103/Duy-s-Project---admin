2aa3c56222fcb2e7a8884eafcde2e3656a63d88a
DucVM <ducalwayslaugh96@gmail.com>
2023-03-08 12:36:07 +0700

    fix missing api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: true,
-    endpoint: 'https://duyprojectapi20230305124117.azurewebsites.net/ap',
+    endpoint: 'https://duyprojectapi20230305124117.azurewebsites.net/api',
     googleAppId:
         '636707412368-aanklm8gkpcb8hnvjps5ad0pqkopan3o.apps.googleusercontent.com',
     facebookAppId: '729974315419896',
     landingUrl:
-        'https://duyprojectapi20230305124117.azurewebsites.net/ap',
+        'https://duyprojectapi20230305124117.azurewebsites.net/api',
 };
